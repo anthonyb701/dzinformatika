f8f2e1c2e5ed4ecdc6097a75979cf2281d99f774
Anthony <xtraderzpro@gmail.com>
2020-04-24 10:26:17 +0300

    main js fix

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
 $(window).load(function() {
   $(".before-after1").twentytwenty({
-    /* before_label: 'Winter', // set 
-    after_label: 'Summer' */
+    before_label: 'Winter', // set 
+    after_label: 'Summer',
   });
-  /* $('.before-slider').slick({
+   $('.slick-elon').slick({
      draggable: false,
-     dots: true,
-     dotsClass: 'before__dots',
-     prevArrow: $('.arrow-left'),
-     nextArrow: $('.arrow-right'),
-     touchMove: false,
-     swipe: false
-  }); */
+     /* dots: true,
+     dotsClass: 'before__dots', */
+  }); 
   $('.slick-slider').slick({
      prevArrow: $('.reviews__btn--prev'),
      nextArrow: $('.reviews__btn--next'),
