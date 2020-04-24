2c78a0e78e8f16febcf698a0188baa76e7b8e24d
Anthony <xtraderzpro@gmail.com>
2020-04-24 10:09:37 +0300

    well done v1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(window).load(function() {
-  $(".before-after").twentytwenty({
-    before_label: 'Без скинали', // set 
-    after_label: 'Со скинали'
+  $(".before-after1").twentytwenty({
+    /* before_label: 'Winter', // set 
+    after_label: 'Summer' */
   });
-  $('.before-slider').slick({
+  /* $('.before-slider').slick({
      draggable: false,
      dots: true,
      dotsClass: 'before__dots',
@@ -11,12 +11,9 @@ $(window).load(function() {
      nextArrow: $('.arrow-right'),
      touchMove: false,
      swipe: false
-  });
+  }); */
   $('.slick-slider').slick({
      prevArrow: $('.reviews__btn--prev'),
      nextArrow: $('.reviews__btn--next'),
   });
-  $('.menu-button').on('click', function(){
-      $('.menu').toggleClass('menu_active');
-  }); 
 });
